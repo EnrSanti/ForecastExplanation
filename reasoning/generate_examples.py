839c6c25150c2171dfb345bd25f16ed8de7779b6
Enrico <santienry@gmail.com>
2026-01-14 16:59:27 +0100

    examples split between morning&aft

--- reasoning/generate_examples.py
+++ reasoning/generate_examples.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from PIL import Image
 import re
 import math
+import shutil
 
 #longmin longmax latmin latmax of FVG and Italy, longmin longmax, latmin latmax
 coordinates=[11,15,44.5,48]
@@ -625,167 +626,9 @@ def merge_into_examples(folder_list_clouds,folder_list_hum, folder_list_temp):
 
         print(f"Wrote example data to {output_path}")
         i+=1
-    background="""
-
-%RAINS
-%rains_at(X) :- forecasted_rain(X, Y), Y > 0.
-%:- rains_at(X), forecasted_rain(X, 0).   % constraint for “only if”
-
-%CLOUD COVER (if)
-sunny_at(X) :- forecasted_sky(X, "sunny").
-sunny_at(X) :- forecasted_sky(X, "mostly_clear").
-partially_sunny_at(X) :- forecasted_sky(X, "partly_cloudy").
-covered_at(X) :- forecasted_sky(X, "mostly_cloudy").
-covered_at(X) :- forecasted_sky(X, "cloudy").
-
-%other implication verse
-:- sunny_at(X), not forecasted_sky(X, "sunny"), not forecasted_sky(X, "mostly_clear").
-:- partially_sunny_at(X), not forecasted_sky(X, "partly_cloudy").
-:- covered_at(X), not forecasted_sky(X, "mostly_cloudy"), not forecasted_sky(X, "cloudy").
-
-%only one is true
-:- sunny_at(X), partially_sunny_at(X).
-:- sunny_at(X), covered_at(X).
-:- partially_sunny_at(X), covered_at(X).
-
-
-cloud(C,L,H) :- cloud_at_100m_covers(C,_,H),  L=100.
-cloud(C,L,H) :- cloud_at_750m_covers(C,_,H),  L=750.
-cloud(C,L,H) :- cloud_at_1_4km_covers(C,_,H), L=1400.
-cloud(C,L,H) :- cloud_at_3km_covers(C,_,H),   L=3000.
-cloud(C,L,H) :- cloud_at_5_5km_covers(C,_,H), L=5500.
-cloud(C,L,H) :- cloud_at_9km_covers(C,_,H),   L=9000.
-
-covered_at_hour(C,H) :-
-    cloud(C,L1,H),
-    cloud(C,L2,H),
-    L1!=L2.
-
-city_covered_at_least(C,1) :-
-    covered_at_hour(C,H1).
-
-city_covered_at_least(C,2) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    H1 != H2.
-
-city_covered_at_least(C,3) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    covered_at_hour(C,H3),
-    H1 != H2, H1 != H3, H2 != H3.
-
-city_covered_at_least(C,4) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    covered_at_hour(C,H3),
-    covered_at_hour(C,H4),
-    H1 != H2, H1 != H3, H1 != H4,
-    H2 != H3, H2 != H4,
-    H3 != H4.
-
-city_covered_at_least(C,5) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    covered_at_hour(C,H3),
-    covered_at_hour(C,H4),
-    covered_at_hour(C,H5),
-    H1 != H2, H1 != H3, H1 != H4, H1 != H5,
-    H2 != H3, H2 != H4, H2 != H5,
-    H3 != H4, H3 != H5,
-    H4 != H5.
-
-city_covered_at_least(C,6) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    covered_at_hour(C,H3),
-    covered_at_hour(C,H4),
-    covered_at_hour(C,H5),
-    covered_at_hour(C,H6),
-    H1 != H2, H1 != H3, H1 != H4, H1 != H5, H1 != H6,
-    H2 != H3, H2 != H4, H2 != H5, H2 != H6,
-    H3 != H4, H3 != H5, H3 != H6,
-    H4 != H5, H4 != H6,
-    H5 != H6.
-
-city_covered_at_least(C,7) :-
-    covered_at_hour(C,H1),
-    covered_at_hour(C,H2),
-    covered_at_hour(C,H3),
-    covered_at_hour(C,H4),
-    covered_at_hour(C,H5),
-    covered_at_hour(C,H6),
-    covered_at_hour(C,H7),
-    H1 != H2, H1 != H3, H1 != H4, H1 != H5, H1 != H6, H1 != H7,
-    H2 != H3, H2 != H4, H2 != H5, H2 != H6, H2 != H7,
-    H3 != H4, H3 != H5, H3 != H6, H3 != H7,
-    H4 != H5, H4 != H6, H4 != H7,
-    H5 != H6, H5 != H7,
-    H6 != H7.
-
-city_not_covered_more_than(C,7) :- not city_covered_at_least(C,7), location(C). %placeholder
-city_not_covered_more_than(C,6) :- not city_covered_at_least(C,7), location(C). 
-city_not_covered_more_than(C,5) :- not city_covered_at_least(C,6), location(C). 
-city_not_covered_more_than(C,4) :- not city_covered_at_least(C,5), location(C).
-city_not_covered_more_than(C,3) :- not city_covered_at_least(C,4), location(C).
-city_not_covered_more_than(C,2) :- not city_covered_at_least(C,3), location(C).
-city_not_covered_more_than(C,1) :- not city_covered_at_least(C,2), location(C).
-
-
-time(0..23).                    
-lv(1..7).
-
-               
-location(sappada_forni_villa).
-location(pontebba_tarvisio).
-location(lignano_grado).
-location(barcis).
-location(udine_palamnova).
-location(gorizia).
-location(trieste).
-location(gemona_stolvizza).
-location(pordenone).
-
-
-coverage("cloudy"). 
-coverage("partly_cloudy"). 
-coverage("mostly_cloudy"). 
-coverage("mostly_clear"). 
-coverage("sunny"). 
-
-
-%CLOUD COVER (if)
-sunny_at(X) :- forecasted_sky(X, "sunny").
-sunny_at(X) :- forecasted_sky(X, "mostly_clear").
-partially_sunny_at(X) :- forecasted_sky(X, "partly_cloudy").
-covered_at(X) :- forecasted_sky(X, "mostly_cloudy").
-covered_at(X) :- forecasted_sky(X, "cloudy").
-
-%other implication verse
-:- sunny_at(X), not forecasted_sky(X, "sunny"), not forecasted_sky(X, "mostly_clear").
-:- partially_sunny_at(X), not forecasted_sky(X, "partly_cloudy").
-:- covered_at(X), not forecasted_sky(X, "mostly_cloudy"), not forecasted_sky(X, "cloudy").
-
-%only one is true
-:- sunny_at(X), partially_sunny_at(X).
-:- sunny_at(X), covered_at(X).
-:- partially_sunny_at(X), covered_at(X).
-
-#maxv(1).
-#modeh(forecasted_sky(var(location),const(coverage))).
-
-#modeh(forecasted_sky(const(location),const(coverage))).
-#modeb(city_covered_at_least(var(location),const(lv))).
-#modeb(city_not_covered_more_than(var(location),const(lv))).
-
-#modeb(city_covered_at_least(const(location),const(lv))).
-#modeb(city_not_covered_more_than(const(location),const(lv))).
-
-%#bias("penalty(1, body(X)) :- in_body(X).").
-
-    """
-    with open(output_folder+"/bg.las", 'w') as f_out:
-        f_out.write(background)
+    #copy in output_folder+"/bg.las" the file output_folder+"/../bg.las"
+    
+    shutil.copyfile("./reasoning/bg.las", output_folder+"/bg.las")
 
 def compute_negative_facts(line):
 
