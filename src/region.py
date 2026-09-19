584057a3fdf879d10400fdc4e1c4c33b0372a63d
Elius <elia.cal24@gmail.com>
2026-09-19 15:38:11 +0200

    Changed default city names to match arpa

--- src/region.py
+++ src/region.py
@@ -81,13 +81,13 @@ _PRESETS = {
 }
 
 CITIES = {
-    "Barcis": {"lat": 46.1906756, "lon": 12.5544384},
-    "Sappada_forni_Villa": {"lat": 46.466194, "lon": 12.876158},
-    "Pontebba_Tarvisio": {"lat": 46.503154, "lon": 13.475782},
-    "Gemona_Stolivizza": {"lat": 46.302533, "lon": 13.262085},
-    "Udine_Palmanova": {"lat": 46.0627018, "lon": 13.2181238},
+    "Prealpi Carniche": {"lat": 46.1906756, "lon": 12.5544384},
+    "Alpi Carniche": {"lat": 46.466194, "lon": 12.876158},
+    "Alpi Giulie": {"lat": 46.503154, "lon": 13.475782},
+    "Prealpi Giulie": {"lat": 46.302533, "lon": 13.262085},
+    "Udine": {"lat": 46.0627018, "lon": 13.2181238},
     "Trieste": {"lat": 45.6514457, "lon": 13.7608539},
     "Gorizia": {"lat": 45.9469495, "lon": 13.5973763},
-    "Lignano_Grado": {"lat": 45.80574, "lon": 13.16546},
+    "Lignano": {"lat": 45.80574, "lon": 13.16546},
     "Pordenone": {"lat": 45.9560557, "lon": 12.6453929},
 }
